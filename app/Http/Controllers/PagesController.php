3a201a3ad5d6579044af6d3ac1f815546099b44f
Abu-Al Mahadi <mohammed.abumahadi@northsouth.edu>
2020-06-22 00:54:27 +0600

    interface_design

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index(){
+        return view('pages.index');
+    }
+
+    public function categories(){
+        return view('pages.categories');
+    }
+
+    public function check_out(){
+        return view('pages.check_out');
+    }
+
+    public function contact(){
+        return view('pages.contact');
+    }
+
+    public function product_page(){
+        return view('pages.product_page');
+    }
+
+    public function shopping_cart(){
+        return view('pages.shopping_cart');
+    }
+
+    public function blog(){
+        return view('pages.blog');
+    }
+
+    public function single_blog(){
+        return view('pages.single_blog');
+    }
+
+    public function single_product(){
+        return view('pages.single_product');
+    }
+
+    public function tracking(){
+        return view('pages.tracking');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PagesController@index')->name('index');
+Route::get('/categories', 'PagesController@categories')->name('categories');
+Route::get('/check_out', 'PagesController@check_out')->name('check_out');
+Route::get('/contact', 'PagesController@contact')->name('contact');
+Route::get('/product_page', 'PagesController@product_page')->name('product_page');
+Route::get('/shopping_cart', 'PagesController@shopping_cart')->name('shopping_cart');
+Route::get('/blog', 'PagesController@blog')->name('blog');
+Route::get('/single_blog', 'PagesController@single_blog')->name('single_blog');
+Route::get('/single_product', 'PagesController@single_product')->name('single_product');
+Route::get('/tracking', 'PagesController@tracking')->name('tracking');
+
